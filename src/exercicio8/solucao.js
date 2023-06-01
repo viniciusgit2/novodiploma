7aff3baa1d88d6e17309c9a1b8b003ffc26c067f
viniciusgit2 <4xv6patelfin@gmail.com>
2023-06-01 09:34:11 -0300

    com integracao entre formatos

---
+++ src/exercicio8/solucao.js
@@ -0,0 +1,7 @@
+function carregar(){
+var msg=window.document.getElementById('..//exercicio8/principal.html/msg')
+var img= window.document.getElementById('..//exercicio8//principal.html/foto')
+var data=new Date()
+var hora=data.getHours
+msg.innerHTML(`agora são ${hora}`)
+}
