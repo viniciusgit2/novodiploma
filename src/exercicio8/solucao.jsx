5599bcdac2450b9647400875bbe6dbd36f22c5e2
viniciusgit2 <4xv6patelfin@gmail.com>
2023-06-02 10:39:42 -0300

    quase lá pagina completa

--- src/exercicio8/solucao.jsx
+++ src/exercicio8/solucao.jsx
@@ -1,14 +1,28 @@
 function carregar(){
     var foto =window.document.getElementById('foto')
     var data2= new Date()
-var minuto= data2.getMinutes()
+    var sex=Boolean 
 var hora= data2.getHours() 
-
 var msg=window.document.getElementById('msg')
 var hora=new Date()
 var relogio= hora.getHours() 
 msg.innerHTML=`agora é pra ser ${relogio} `
+var ver=Date()
+if(ver.length==0 -Number (ver)){
+    window.alert("verifique os dados")
 
+}
+else{
+    window.alert("ok")
+
+}
+if(sex='m'){
+    innerHTML=`seu   ${sex} é masculino `
+}
+else{
+    innerHTML=`seu   ${sex} é femenino `
+
+}
 
 if (relogio<12){
     foto.src="../../public/base manha.png"
@@ -19,4 +33,8 @@ if (relogio<12){
     else if (relogio>18){
     foto.src="../../public/base noite.png"
     }
+    
+}
+function carregar2(){
+
 }
